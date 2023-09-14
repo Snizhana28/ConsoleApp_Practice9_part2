a472c8fc47545c9811adb59632ea64a91accbeda
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-09-14 22:35:48 +0300

    Done task1

--- Internal/Program.cs
+++ Internal/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
+            string[] array = new string[] { "Hello!", "My", "Name", "Is", "Snizhana", "I", "am", "a", "student" };
 
+            
+            var result = array.OrderByDescending(x => x.Length);
+            var result2 = array.OrderBy(x => x.Length);
+
+            Console.WriteLine("Sorting is performed by descending order:");
+            Console.WriteLine(string.Join(", ", result));
+            Console.WriteLine("Sorting is performed by ascending order:");
+            Console.WriteLine(string.Join(", ", result2));
         }
     }
 }
